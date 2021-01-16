587048ad05e667cfa037940eafcd612f2c670f0b
Sidhved <66831453+Sidhved@users.noreply.github.com>
2021-01-17 01:26:00 +0530

    #1 for single var LR

---
+++ 1. Linear Regression/plotData.m
@@ -0,0 +1,6 @@
+function plotData(x,y)
+  figure; % opens a new figure window
+  plot(x, y, 'rx', 'MarkerSize', 10);
+  ylabel('Profit in $10,000s')
+  xlabel('Population of Cities in 10,000s')  
+endfunction
