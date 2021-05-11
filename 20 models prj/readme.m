5f1be62465d12ad98bccf92ab585e8e739b225d0
Sidhved <66831453+Sidhved@users.noreply.github.com>
2021-05-11 16:36:24 +0530

    Update readme.m

--- 20 models prj/readme.m
+++ 20 models prj/readme.m
@@ -1,1 +1,4 @@
+<h1> 20 models Training prj </h1>
+
 This folder consists of all the data and code for implemention 20 unique ML models on a dataset.
+The code can be found in executable format in the google colab by following the link --> [Link]:"https://colab.research.google.com/drive/1WXlmd27U-w7RYKNn647NE5_nIBVsCdnX"
